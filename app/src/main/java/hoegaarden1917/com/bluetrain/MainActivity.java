8d2abe390ab65889ab9fc4faed794f6087768544
Yuji Ogihara <yuji.ogihara.85@gmail.com>
2017-04-17 13:51:47 +0900

    Initial commit

---
+++ app/src/main/java/hoegaarden1917/com/bluetrain/MainActivity.java
@@ -0,0 +1,134 @@
+package hoegaarden1917.com.bluetrain;
+
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonWhistle, buttonSomething ;
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonWhistle = (Button) findViewById(R.id.button_whistle);
+
+
+        // ボタンがクリックされた時に呼び出されるコールバックリスナーを登録します
+        buttonWhistle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ボタンがクリックされた時に呼び出されます
+                Button button = (Button) v;
+
+            }
+        });
+        buttonWhistle.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    audioPlay();
+
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    audioStop();
+                }
+                return false;
+            }
+        });
+
+        buttonSomething = (Button) findViewById(R.id.button_something);
+        // ボタンがクリックされた時に呼び出されるコールバックリスナーを登録します
+        buttonSomething.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ボタンがクリックされた時に呼び出されます
+                Button button = (Button) v;
+
+
+            }
+        });
+    }
+
+    private boolean audioSetup(){
+        boolean fileCheck = false;
+
+        // インタンスを生成
+        mediaPlayer = new MediaPlayer();
+
+        //音楽ファイル名, あるいはパス
+        String filePath = "whistle_00_01.mp3";
+
+        try {
+            // assetsから mp3 ファイルを読み込み
+            AssetFileDescriptor afdescripter = getAssets().openFd(filePath);
+            // MediaPlayerに読み込んだ音楽ファイルを指定
+            mediaPlayer.setDataSource(afdescripter.getFileDescriptor(),
+                    afdescripter.getStartOffset(),
+                    afdescripter.getLength());
+            // 音量調整を端末のボタンに任せる
+//            setVolumeControlStream(AudioManager.STREAM_MUSIC);
+//            mediaPlayer.setLooping(true);
+            mediaPlayer.prepare();
+            fileCheck = true;
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        return fileCheck;
+    }
+
+    private void audioPlay() {
+
+        if (mediaPlayer == null) {
+            // audio ファイルを読出し
+            if (audioSetup()){
+                Toast.makeText(getApplication(), "Rread audio file", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(getApplication(), "Error: read audio file", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+        else{
+            // 繰り返し再生する場合
+            mediaPlayer.stop();
+            mediaPlayer.reset();
+            // リソースの解放
+            mediaPlayer.release();
+        }
+
+        // 再生する
+        mediaPlayer.start();
+
+        // 終了を検知するリスナー
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            public void onCompletion(MediaPlayer mp) {
+                Log.d("debug","end of audio");
+
+            }
+        });
+    }
+
+    private void audioStop() {
+        // 再生終了
+        mediaPlayer.stop();
+        // リセット
+        mediaPlayer.reset();
+        // リソースの解放
+        mediaPlayer.release();
+
+        mediaPlayer = null;
+    }
+}
